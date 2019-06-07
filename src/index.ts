884fbe90b561ecc158fcb3bd0b2eb3a3445cae36
Akshit Grover <akshit.grover2016@gmail.com>
2019-06-08 00:34:03 +0530

    Server: Set bodyParser middlewares

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,15 @@
 import * as express from 'express';
+import * as bodyParser from 'body-parser';
 
 import env from './env';
 
 const app: express.Express = express();
+
+/*
+  Set request body parsing middlewares
+*/
+app.use(bodyParser.json()); // Parse JSON reqest body
+app.use(bodyParser.urlencoded({ extended: true })); // Parse URL params
 
 /*
   Set default ENVs
