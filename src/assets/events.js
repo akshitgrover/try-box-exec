e61ef0c1f69443b7bf8e0966131d1507347c0e30
Akshit Grover <akshit.grover2016@gmail.com>
2019-06-17 02:37:36 +0530

    Run: Add http request function

--- src/assets/events.js
+++ src/assets/events.js
@@ -1,7 +1,5 @@
 var codeSection = document.getElementsByClassName('code-section');
 
-console.log(codeSection[0].innerHTML);
-
 codeSection[0].addEventListener('keydown', function (e) {
   if (e.which.toString() == '9') {
     e.preventDefault();
@@ -15,5 +13,31 @@ codeSection[0].addEventListener('keydown', function (e) {
     range.setEndAfter(tabNode);
     s.removeAllRanges();
     s.addRange(range);
+  }
+});
+
+var run = document.getElementsByClassName('run');
+
+run[0].addEventListener('click', function (e) {
+  var code = document.getElementById('code').innerText;
+  var testCase = document.getElementById('testCase').innerText;
+  var lang = document.getElementById('lang');
+  var language = lang.options[lang.selectedIndex].value;
+  var timeOut = document.getElementById('timeOut').innerText.trim();
+  code = code.replace(/\u00a0/g, ' ');
+
+  var body = {
+    code: code,
+    language: language,
+    testCase: testCase,
+    timeOut: timeOut,
+    socketID: socketID.id,
+  };
+  var req = new XMLHttpRequest();
+  req.open('POST', '/execute', true);
+  req.setRequestHeader('Content-Type', 'application/json');
+  req.send(JSON.stringify(body));
+  req.onreadystatechange = function () {
+    console.log(req.readyState);
   }
 });
